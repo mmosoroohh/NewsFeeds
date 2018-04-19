3b6e547c0e9ca31d3ad2dbbaae5aced202b9fc1f
Arnold M. Osoro <36207509+mmosoroohh@users.noreply.github.com>
2018-04-20 00:42:39 +0300

    NewsFeeds python test files

---
+++ data.py
@@ -0,0 +1,32 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio eaque minus sit, veniam autem soluta dolorem consequuntur assumenda hic inventore facere perferendis, reiciendis dignissimos sapiente quisquam iste unde ut consequatur.',
+            'author': 'Arnold Osoro',
+            'create_date': '04-12-2018'
+        },
+         {
+            'id': 2,
+            'title':'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio eaque minus sit, veniam autem soluta dolorem consequuntur assumenda hic inventore facere perferendis, reiciendis dignissimos sapiente quisquam iste unde ut consequatur.',
+            'author': 'Brian Osoro',
+            'create_date': '04-12-2018'
+        },
+         {
+            'id': 3,
+            'title':'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio eaque minus sit, veniam autem soluta dolorem consequuntur assumenda hic inventore facere perferendis, reiciendis dignissimos sapiente quisquam iste unde ut consequatur.',
+            'author': 'Carolyne Kerubo',
+            'create_date': '04-12-2018'
+        },
+         {
+            'id': 4,
+            'title':'Article Four',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio eaque minus sit, veniam autem soluta dolorem consequuntur assumenda hic inventore facere perferendis, reiciendis dignissimos sapiente quisquam iste unde ut consequatur.',
+            'author': 'Claire Moraa',
+            'create_date': '04-12-2018'
+        }
+    ]
+    return articles
